9094686a72478c9211d60c4b8b3be6bfb83ef07e
Space <space@mbp-space.speedport.ip>
2026-07-19 22:12:05 +0200

    images added

--- scripts/main.js
+++ scripts/main.js
@@ -3,7 +3,7 @@
 // =====================
 import { BRANDS } from './brands.js?v=20260712-brands';
 import { BRAND_SNAPSHOT_CONFIGS } from './brand-snapshot-configs.js';
-import { BRAND_NEW_GAMES } from './brand-new-games.js?v=20260719-brand-games-25';
+import { BRAND_NEW_GAMES } from './brand-new-games.js?v=20260719-brand-games-27';
 import { COUNTRIES } from './countries.js';
 
 // =====================
